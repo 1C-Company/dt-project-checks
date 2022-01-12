df63526196602714c6c5740f3a684fd27885da04
Dmitriy Marmyshev <dmar@1c.ru>
2022-01-12 18:24:20 +0200

    #12 Квик-фикс по проверки путей к данным на форме
    - удаление битой ссылки в пути к данным
    - удаление элемента формы с битой ссылкой

---
+++ bundles/com.e1c.dt.check.form/src/com/e1c/dt/check/internal/form/fix/DataPathRemoveFix.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (C) 2022, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+package com.e1c.dt.check.internal.form.fix;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import com._1c.g5.v8.dt.form.model.ExtInfo;
+import com._1c.g5.v8.dt.form.model.FormItem;
+import com.e1c.dt.check.form.DataPathReferredObjectCheck;
+import com.e1c.dt.check.internal.form.CorePlugin;
+import com.e1c.g5.v8.dt.check.qfix.IFixSession;
+import com.e1c.g5.v8.dt.check.qfix.components.BasicModelFixContext;
+import com.e1c.g5.v8.dt.check.qfix.components.MultiVariantModelBasicFix;
+import com.e1c.g5.v8.dt.check.qfix.components.QuickFix;
+
+/**
+ * The multi-variant fix for {@link DataPathReferredObjectCheck} allows to remove bad data path
+ * and remove form item with this data path.
+ */
+@QuickFix(checkId = "form-data-path", supplierId = CorePlugin.PLUGIN_ID)
+public class DataPathRemoveFix
+    extends MultiVariantModelBasicFix
+{
+
+    @Override
+    protected void buildVariants()
+    {
+        VariantBuilder builder = VariantBuilder.create(this);
+
+        builder
+            .description(Messages.DataPathRemoveFix_Remove_data_path_title,
+                Messages.DataPathRemoveFix_Remove_data_path_description)
+            .change(this::removeDataPath)
+            .build();
+
+        builder
+            .description(Messages.DataPathRemoveFix_Remove_form_item_title,
+                Messages.DataPathRemoveFix_Remove_form_item_description)
+            .change(this::removeFormItem)
+            .build();
+    }
+
+    private void removeDataPath(EObject object, EStructuralFeature feature, BasicModelFixContext context,
+        IFixSession session)
+    {
+        removeObject(object);
+    }
+
+    private void removeFormItem(EObject object, EStructuralFeature feature, BasicModelFixContext context,
+        IFixSession session)
+    {
+        EObject item = object.eContainer();
+        if (item instanceof ExtInfo)
+        {
+            item = item.eContainer();
+        }
+        if (!(item instanceof FormItem))
+        {
+            return;
+        }
+
+        removeObject(item);
+    }
+
+    private void removeObject(EObject object)
+    {
+        EObject parent = object.eContainer();
+        EStructuralFeature containing = object.eContainmentFeature();
+        if (containing == null || parent == null)
+        {
+            return;
+        }
+
+        if (containing.isMany())
+        {
+            List<?> list = (List<?>)parent.eGet(containing);
+            int index = list.indexOf(object);
+            if (index > -1)
+            {
+                list.remove(index);
+            }
+        }
+        else
+        {
+            parent.eUnset(containing);
+        }
+    }
+
+}
---
+++ bundles/com.e1c.dt.check.form/src/com/e1c/dt/check/internal/form/fix/Messages.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (C) 2022, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial API and implementation
+ *******************************************************************************/
+package com.e1c.dt.check.internal.form.fix;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * @author Dmitriy Marmyshev
+ *
+ */
+class Messages
+    extends NLS
+{
+    private static final String BUNDLE_NAME = "com.e1c.dt.check.internal.form.fix.messages"; //$NON-NLS-1$
+    public static String DataPathRemoveFix_Remove_data_path_description;
+    public static String DataPathRemoveFix_Remove_data_path_title;
+    public static String DataPathRemoveFix_Remove_form_item_description;
+    public static String DataPathRemoveFix_Remove_form_item_title;
+    static
+    {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages()
+    {
+    }
+}
