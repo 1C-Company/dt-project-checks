e200e97e34596004fab399e8c8fde667751d19ab
Alexander Tretyakevich <tretyakevich@gmail.com>
2022-06-24 17:36:59 +0700

    #18 проверка ссылочной целостности метаданных (#29)
    Добавлена проверка на ссылочную целостность MD-объектов в конфигурации.
    Добавлена проверка md-ссылок в составе плана обмена и автономной конфигурации
    
    Co-authored-by: Alexander Tretyakevich <alexander.tretyakevich@xored.com>
    Co-authored-by: Andrey Volkov <avol@1c.ru>

---
+++ bundles/com.e1c.dt.check.md/markdown/md-reference-intergrity.md
@@ -0,0 +1,10 @@
+# Metadata referential integrity check
+
+Checks the referential integrity of metadata objects in a configuration. Any lost reference which potentially prevents the configuration from the deployment and/or normal work in 1C:Enterprise is a subject for this check
+
+## Noncompliant Code Example
+
+## Compliant Solution
+
+## See
+
---
+++ bundles/com.e1c.dt.check.md/src/com/e1c/dt/check/internal/md/Messages.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (C) 2022, 1C-Soft LLC and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     1C-Soft LLC - initial implementation
+ *******************************************************************************/
+package com.e1c.dt.check.internal.md;
+
+import org.eclipse.osgi.util.NLS;
+
+/**
+ * Messages.
+ *
+ * @author Alexander Tretyakevich
+ */
+final class Messages
+    extends NLS
+{
+    private static final String BUNDLE_NAME = "com.e1c.dt.check.internal.md.messages"; //$NON-NLS-1$
+
+    public static String MdReferenceIntegrity_LostReference_Single;
+    public static String MdReferenceIntegrity_LostReference_Many;
+    public static String MdReferenceIntegrity_Title;
+    public static String MdReferenceIntegrity_Description;
+
+    static
+    {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages()
+    {
+    }
+}
