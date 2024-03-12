5ce7cecf87cbb0ea5904f94eadf2c9309f237a4c
Maxim Dzyuba <dzym@1c.ru>
2024-03-12 13:03:19 +0300

    G5V8DT-24042 Починить все тесты, падающие в публичных плагинах

--- tests/com.e1c.dt.check.form.itests/src/com/e1c/dt/internal/check/form/itests/SingleProjectWithExtReadOnlyCheckTestBase.java
+++ tests/com.e1c.dt.check.form.itests/src/com/e1c/dt/internal/check/form/itests/SingleProjectWithExtReadOnlyCheckTestBase.java
@@ -15,8 +15,11 @@ package com.e1c.dt.internal.check.form.itests;
 import org.eclipse.core.runtime.CoreException;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.ClassRule;
 
 import com._1c.g5.v8.dt.core.platform.IDtProject;
+import com._1c.g5.v8.dt.platform.version.Version;
+import com._1c.g5.v8.dt.testing.TestingPlatformSupport;
 import com.e1c.g5.v8.dt.testing.check.SingleProjectReadOnlyCheckTestBase;
 
 /**
@@ -27,6 +30,12 @@ import com.e1c.g5.v8.dt.testing.check.SingleProjectReadOnlyCheckTestBase;
 public abstract class SingleProjectWithExtReadOnlyCheckTestBase
     extends SingleProjectReadOnlyCheckTestBase
 {
+    @ClassRule
+    public static TestingPlatformSupport testingPlatformSupport =
+        new TestingPlatformSupport(Version.V8_3_8, Version.V8_3_9, Version.V8_3_10, Version.V8_3_11, Version.V8_3_12,
+            Version.V8_3_13, Version.V8_3_14, Version.V8_3_15, Version.V8_3_16, Version.V8_3_17, Version.V8_3_18,
+            Version.V8_3_19, Version.V8_3_20, Version.V8_3_21, Version.V8_3_22, Version.V8_3_23, Version.V8_3_24);
+
     private static final ThreadLocal<IDtProject> dtProjectExtHolder = new ThreadLocal<>();
     private static Object lock = new Object();
 
@@ -71,4 +80,3 @@ public abstract class SingleProjectWithExtReadOnlyCheckTestBase
      */
     protected abstract String getTestConfigurationExtName();
 }
-
